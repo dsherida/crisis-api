6474a8baa5ec5682724689fda048a132be42d405
Doug Sheridan <dsherida@asu.edu>
2018-10-16 21:57:28 -0700

    More cleanup

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-hike = require('./routes/hike');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-app.get('/hikes', hike.index);
-app.post('/add_hike', hike.add_hike);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +19,6 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/hike.js
+++
@@ -1,6 +0,0 @@
-exports.index = function(req, res) {
-    res.json({title: 'My Hiking Log'});
-};
-
-exports.add_hike = function(req, res) {
-};
