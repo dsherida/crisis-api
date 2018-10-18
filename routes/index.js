5bebb53291dda18a535f243f028209aee06eaacf
Doug Sheridan <dsherida@asu.edu>
2018-10-17 19:20:01 -0700

    Add /login

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+const MembersTableName = 'Members';
+
+class Member {
+    constructor(height, width) {
+        this.username = height;
+        this.password = width;
+    }
+}
+
+const MembersCollection = (req) => {
+    return req.app.db.collection(MembersTableName);
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  req.app.db.collection('Members').insertOne({ 'name': "Jim" },
-      function(err, r) {
-        console.log("Woohoo!");
-      }
-  )
-
   res.json({ message: 'Hello Crisis!' });
 });
 
+router.post('/register', function(req, response, next) {
+    let member = new Member(
+        req.body.username,
+        req.body.password
+    );
+
+    const members = MembersCollection(req);
+
+    members.findOne({username: req.body.username}, function (err, res) {
+        if (err) response.json(err);
+
+        if (!res) {
+            members.insertOne(member,
+                function (err, r) {
+                    if (err) res.json({ error: err });
+
+                    response.json(member);
+                }
+            );
+        } else {
+            response.json({ message: 'Username is already in use'});
+        }
+    });
+});
+
 router.get('/login', function(req, res, next) {
-  res.json({ message: 'Todo: Log user in'});
+  res.json({ body: req.body });
 });
 
 module.exports = router;
